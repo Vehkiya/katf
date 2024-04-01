e1eaefc2fe753a988f8806f3a0a477e9f8b33ff9
Sergiu Ababii <l33tbird@gmail.com>
2024-04-01 17:00:17 +0300

    Added more steps

--- src/test/kotlin/app/kerrlab/steps/StepDefs.kt
+++ src/test/kotlin/app/kerrlab/steps/StepDefs.kt
@@ -1,11 +1,29 @@
 package app.kerrlab.steps
 
 import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.jupiter.api.Assertions
 
 class StepDefs {
+
+    private var pickles = 0
 
     @Given("{int} pickles")
     fun givenSomePickles(number: Int) {
         println("I got $number pickles")
+        pickles = number
+    }
+
+    @When("{int} more pickles are added")
+    fun picklesAreAdded(number: Int) {
+        println("I add $number more pickles")
+        pickles += number
+    }
+
+    @Then("I have {int} pickles in total")
+    fun totalPickles(total: Int) {
+        println("I have ${total} pickles total")
+        Assertions.assertEquals(total, pickles, "I expected $total pickles but I only have $pickles pickles")
     }
 }
--- src/test/resources/features/Test.feature
+++ src/test/resources/features/Test.feature
@@ -3,7 +3,8 @@ Feature: Kotlin Cucumber Demo
   @Run
   Scenario: Demo Scenario
     Given 2 pickles
-    And 4 pickles
+    When 3 more pickles are added
+    Then I have 5 pickles in total
 
   Scenario: scenario without tag
     Given 3 pickles
